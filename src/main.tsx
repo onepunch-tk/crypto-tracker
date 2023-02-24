afb5901aade50b28e1442554ef6998371ebc0d1f
TaekyungHa <86tkstar@gmail.com>
2023-02-24 22:26:03 +0900

    add ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {GlobalStyle} from "./styles/reset-css";
+import {ThemeProvider} from "styled-components";
+import {theme} from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <>
         <GlobalStyle/>
-        <App/>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
     </>,
 )
