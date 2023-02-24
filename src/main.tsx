e297447ed3f44a3bf608004959641d0ae85f4498
TaekyungHa <86tkstar@gmail.com>
2023-02-24 22:40:49 +0900

    create Router.tsx, Root.tsx and add RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import {GlobalStyle} from "./styles/reset-css";
 import {ThemeProvider} from "styled-components";
 import {theme} from "./styles/theme";
+import {RouterProvider} from 'react-router-dom';
+import {router} from "./routes/Router";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <>
         <GlobalStyle/>
         <ThemeProvider theme={theme}>
-            <App/>
+            <RouterProvider router={router} />
         </ThemeProvider>
-    </>,
+    </>
 )
---
+++ src/routes/Root.tsx
@@ -0,0 +1,7 @@
+function Root() {
+    return (
+        <h1>Root Component</h1>
+    );
+}
+
+export default Root;
