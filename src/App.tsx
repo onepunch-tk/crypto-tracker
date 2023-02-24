0770aa576634edd437632081780ebe6dc0c0672d
TaekyungHa <86tkstar@gmail.com>
2023-02-24 22:15:32 +0900

    add reset-css.ts and implement global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
 
   return (
-    <></>
+    <>
+      <h1>App</h1>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {GlobalStyle} from "./styles/reset-css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <>
-    <App />
-  </>,
+    <>
+        <GlobalStyle/>
+        <App/>
+    </>,
 )
